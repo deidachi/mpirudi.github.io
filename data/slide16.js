97128fe69eb87ea2584c6aa3d768c2c96594a70c
deidachi <33218474+deidachi@users.noreply.github.com>
2023-09-18 09:28:27 +0700

    Add files via upload

---
+++ data/slide16.js
@@ -0,0 +1,13 @@
+(function(){var loadHandler=window['sl_{4A35EE8C-AFAD-4403-96BE-2BF6A4A5C128}'];loadHandler&&loadHandler(15, '<div id="spr0_800889f"><div id="spr1_800889f" class="kern slide"><img id="img7_800889f" src="data/img3.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_800889f" class="kern slide"><div id="spr3_800889f" style="left:139px;top:167px;"><img id="img0_800889f" src="data/img53.png" width="330" height="39" alt="Sumber Materi\
+"/></div><div id="spr4_800889f" style="left:375.714px;top:28.315px;"><img id="img1_800889f" src="data/img54.png" width="430" height="124" alt="" style="left:-0.23px;top:0.193px;"/></div><div id="spr5_800889f" style="left:705px;top:251px;"><img id="img2_800889f" src="data/img55.png" width="500" height="162" alt="www.freepik.com\
+www.shutterstock.com\
+Kelasanimasi.com\
+www.sekolahinformatika.com\
+"/></div><div id="spr6_800889f" style="left:705px;top:460px;"><img id="img3_800889f" src="data/img56.png" width="534" height="162" alt="Sumber Audio \
+soundcloud.com/roa_music1031 \
+Youtube : Kemendikbud RI\
+"/></div><div id="spr7_800889f" style="left:139px;top:255px;"><img id="img4_800889f" src="data/img57.png" width="463" height="342" alt="Ilmu Pengetahuan Sosialuntuk SMP/MTsKelas VII Penerbit Yudhistira Tahun 2022, Jakarta\
+\
+Buku BSE Ilmu Pengetahuan Sosial Siswa Kelas VII, 2022, Jakarta\
+"/></div><div id="spr8_800889f" style="left:705px;top:167px;"><img id="img5_800889f" src="data/img58.png" width="361" height="39" alt="Sumber Gambar\
+"/></div><div id="spr9_800889f" style="left:1169.193px;top:10.036px;"><a id="hl0_800889f" href="#" onclick="document.getElementById(\'coreSpr_3662\').getCore().processTriggerEffect(this);document.getElementById(\'coreSpr_3662\').getCore().gotoSlide(2, this);return false;"><img id="img6_800889f" src="data/img28.png" width="75.591" height="75.591" alt=""/></a></div></div></div>', '{"s":[]}');})();
